fbdf08ce38ae7756bd92b2abaff89f397f4fbbdd
Yanis <yanis@netkoders.com>
2022-04-27 09:55:49 +0200

    beaucoup de travail

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('btn5').addEventListener('mousemove', (e) => {
+
+    const x = e.pageX - e.target.offsetLeft
+    const y = e.pageY - e.target.offsetTop
+
+    e.target.style.setProperty('--x', `${x}px`);
+    e.target.style.setProperty('--y', `${y}px`);
+  })

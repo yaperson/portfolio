511d4bfa4b389c68a90c973549547a4be3b7599f
yanis <yanis.legrand.1@gmail.com>
2023-02-17 14:58:18 +0100

    Rebuild portfolio in vuejs

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import navbar from './components/navbar.vue';
+
+export default {
+  components: {
+    navbar: navbar
+  }
+}
+</script>
+
+<template>
+  <header>
+    <navbar></navbar>
+  </header>
+
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<style>
+header {
+  display: flex;
+  flex-direction: column;
+  min-height: 20vh;
+}
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <router-link to="/">Home</router-link>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "About",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
